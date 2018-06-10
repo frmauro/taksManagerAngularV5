35e3783a4f1b57a6a385822d11e2c35b62a69d42
francisco <frmauro8@hotmail.com>
2018-06-10 09:02:04 -0300

    create class Task

--- src/app/save-task/save-task.component.ts
+++ src/app/save-task/save-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../Task';
 
 @Component({
   selector: 'app-save-task',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaveTaskComponent implements OnInit {
 
-  description = "Task 001";
+  task: Task = {
+    id: 1,
+    description: "Task 001"
+  } 
   
   constructor() { }
 
